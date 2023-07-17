18497631896686b80e652b9f5c7c00d6317d7cdb
Shan <shanpiv@gmail.com>
2023-07-17 20:14:11 +0300

    Starts

---
+++ src/main/java/com/example/cicdandjunit/CicdAndJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdandjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAndJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAndJunitApplication.class, args);
+	}
+
+}
